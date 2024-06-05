4f51ac4041077d067bc01d8c5a23469522d86100
sookimm <su241383@gmail.com>
2024-06-04 20:04:15 -0400

    Use author and version variables in health check response

--- src/app.js
+++ src/app.js
@@ -1,18 +1,13 @@
-// src/app.js
-
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 const compression = require('compression');
-const passport = require('passport');
-const authenticate = require('./auth'); // Import the strategy and authenticate function
 
 // author and version from our package.json file
 const { author, version } = require('../package.json');
 
 const logger = require('./logger');
 const pino = require('pino-http')({
-  // Use our default logger instance, which is already configured
   logger,
 });
 
@@ -31,12 +26,21 @@ app.use(cors());
 // Use gzip/deflate compression middleware
 app.use(compression());
 
-// Set up our passport authentication middleware
-passport.use(authenticate.strategy());
-app.use(passport.initialize());
+// Define a simple health check route. If the server is running
+// we'll respond with a 200 OK.  If not, the server isn't healthy.
+app.get('/', (req, res) => {
+  // Clients shouldn't cache this response (always request it fresh)
+  // See: https://developer.mozilla.org/en-US/docs/Web/HTTP/Caching#controlling_caching
+  res.setHeader('Cache-Control', 'no-cache');
 
-// Define our routes
-app.use('/', require('./routes'));
+  // Send a 200 'OK' response with info about our repo
+  res.status(200).json({
+    status: 'ok',
+    author, // Use author variable here
+    githubUrl: 'https://github.com/sookimm/fragments.git',
+    version, // Use version variable here
+  });
+});
 
 // Add 404 middleware to handle any requests for resources that can't be found
 app.use((req, res) => {
