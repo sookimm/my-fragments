86f1bd08ffadded0397092ba2f92589a0cc57552
sookimm <su241383@gmail.com>
2024-06-05 01:02:37 -0400

    Add logging the app instance

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-// src/index.js
-
 // Read environment variables from an .env file (if present)
 require('dotenv').config();
 
 // We want to log any crash cases so we can debug later from logs.
 const logger = require('./logger');
 
-const app = require('./app');
+const app = require('./app'); // Ensure 'app' is used
+
+// Log the app instance
+logger.info({ app }, 'App instance loaded');
 
 // If we're going to crash because of an uncaught exception, log it first.
 // https://nodejs.org/api/process.html#event-unhandledrejection
