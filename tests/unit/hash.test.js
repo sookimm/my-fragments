ae5e959c9c1bf2fc4a59617b4495f639a0793dcd
sookimm <su241383@gmail.com>
2024-06-15 12:45:54 -0400

    fix comment

--- tests/unit/hash.test.js
+++ tests/unit/hash.test.js
@@ -6,7 +6,7 @@ describe('Hash Email', () => {
   test('should hash an email correctly', () => {
     const email = 'test@example.com';
     const hashedEmail = hashEmail(email);
-    const expectedHash = '973dfe463ec85785f5f95af5ba3906eedb2d931c24e69824a89ea65dba4e813b'; // 실제 해시 값
+    const expectedHash = '973dfe463ec85785f5f95af5ba3906eedb2d931c24e69824a89ea65dba4e813b'; // Real hash value
     expect(hashedEmail).toBe(expectedHash);
   });
 });
