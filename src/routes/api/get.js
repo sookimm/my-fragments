bf973dd50e9f01fb66924fc6f4314134a97b2b17
sookimm <su241383@gmail.com>
2024-06-24 18:27:21 -0400

    Add GET /fragments/:id/info endpoint

--- src/routes/api/get.js
+++ src/routes/api/get.js
@@ -1,11 +1,21 @@
 // src/routes/api/get.js
 
+const { Fragment } = require('../../model/fragment');
 const logger = require('../../logger');
 
-module.exports = (req, res) => {
-  logger.info('Fetching fragments for user', { user: req.user });
-  res.status(200).json({
-    status: 'ok',
-    fragments: [],
-  });
+module.exports = async (req, res) => {
+  try {
+    logger.info('Fetching fragments for user', { user: req.user });
+    const fragments = await Fragment.byUser(req.user);
+    res.status(200).json({
+      status: 'ok',
+      fragments,
+    });
+  } catch (err) {
+    logger.error('Error fetching fragments for user', { error: err.message });
+    res.status(500).json({
+      status: 'error',
+      error: 'Unable to fetch fragments',
+    });
+  }
 };
