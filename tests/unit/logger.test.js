76f5ee1e6e7065f12067f7db6f3a49658904591a
sookimm <su241383@gmail.com>
2024-07-10 13:41:17 -0400

    Improve Coverage

--- tests/unit/logger.test.js
+++ tests/unit/logger.test.js
@@ -7,11 +7,14 @@ jest.mock('pino');
 describe('Logger', () => {
   let logger;
   let consoleErrorSpy;
+  let consoleInfoSpy;
 
   beforeEach(() => {
     consoleErrorSpy = jest.spyOn(console, 'error').mockImplementation(() => {});
+    consoleInfoSpy = jest.spyOn(console, 'info').mockImplementation(() => {});
     pino.mockImplementation(() => ({
       error: consoleErrorSpy,
+      info: consoleInfoSpy,
     }));
     logger = require('../../src/logger');
   });
@@ -25,5 +28,12 @@ describe('Logger', () => {
     logger.error(errorMessage);
     expect(consoleErrorSpy).toHaveBeenCalled();
     expect(consoleErrorSpy.mock.calls[0][0]).toContain(errorMessage);
+  });
+
+  test('should log info messages', () => {
+    const infoMessage = 'Test info message';
+    logger.info(infoMessage);
+    expect(consoleInfoSpy).toHaveBeenCalled();
+    expect(consoleInfoSpy.mock.calls[0][0]).toContain(infoMessage);
   });
 });
