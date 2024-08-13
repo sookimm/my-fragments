e54043f8cf22912127fa1f1afc33a690abbff12e
sookimm <su241383@gmail.com>
2024-08-13 16:10:46 -0400

    Fix fragments name for localstack

--- scripts/local-aws-setup.sh
+++ scripts/local-aws-setup.sh
@@ -18,11 +18,10 @@ export AWS_SESSION_TOKEN=test
 export AWS_DEFAULT_REGION=us-east-1
 echo "AWS_DEFAULT_REGION=us-east-1"
 
-# Wait for LocalStack to be ready, by inspecting the response from /health, see:
-# https://github.com/localstack/localstack/issues/4904#issuecomment-966315170
+# Wait for LocalStack to be ready, by inspecting the response from healthcheck
 echo 'Waiting for LocalStack S3...'
-until (curl --silent http://localhost:4566/health | grep "\"s3\": \"\(running\|available\)\"" > /dev/null); do
-    sleep 10
+until (curl --silent http://localhost:4566/_localstack/health | grep "\"s3\": \"\(running\|available\)\"" > /dev/null); do
+    sleep 5
 done
 echo 'LocalStack S3 Ready'
 
